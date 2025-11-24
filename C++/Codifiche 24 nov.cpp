8cefce0e7c4770805483b98a10f4a5a67bc4e494
Tiziano <t.gaudenzino@gmail.com>
2025-11-24 19:23:54 +0100

    Massimo pag 93

--- C++/Codifiche 24 nov.cpp
+++ C++/Codifiche 24 nov.cpp
@@ -16,9 +16,31 @@ void Controllo()
     }
 
 }
+void Massimo()
+{
+
+    int num1,num2,num3;
+    cout<<"inserire il primo numero: ";
+    cin>>num1;
+    cout<<"inserire il secondo numero: ";
+    cin>>num2;
+    cout<<"inserire il terzo numero: ";
+    cin>>num3;
+    int max=num1;
+    if (num2>max)
+    {
+        max=num2;
+    }
+    else if (num3>max)
+    {
+        max=num3;
+    }
+    cout<<max;
+}
 int main()
 {
-    Controllo();
+    //Controllo();
+    Massimo();
 }
 
 
